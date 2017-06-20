e4459d8dacdc572222760ed71a9fdf023b01ea63
A763847 <alon.amsalem@gmail.com>
2017-06-20 13:43:29 +0300

    cleanup

---
+++ src/AnyStatus.Monitors.Tests/OpenInBrowserTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace AnyStatus.Integration.Tests
+{
+    [TestClass]
+    public class OpenInBrowserTests
+    {
+        [TestMethod]
+        public void OpenGitHubIssueInBrowser()
+        {
+            var gitHubIssue = new GitHubIssue
+            {
+                IssueNumber = 1,
+                Name = "name",
+                Owner = "owner",
+                Repository = "repository"
+            };
+
+            var processStarter = Substitute.For<IProcessStarter>();
+
+            var handler = new OpenGitHubIssueInBrowser(processStarter);
+
+            handler.Handle(gitHubIssue);
+
+            processStarter.Received().Start(Arg.Any<string>());
+        }
+    }
+}
