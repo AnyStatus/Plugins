3ec4440b6944f12502639534cbee678b135540a7
Alon Amsalem <alon@anystat.us>
2018-12-06 15:57:21 +0200

    added local computer Processes and Threads metrics

---
+++ src/AnyStatus.Plugins/Widgets/Metrics/Processes.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnyStatus.Plugins.Widgets.Metrics
+{
+    [DisplayColumn("Metrics")]
+    [DisplayName("Processes")]
+    [Description("The number of processes running on local computer.")]
+    public class Processes : PerformanceCounter
+    {
+        public Processes()
+        {
+            CategoryName = "System";
+            CounterName = "Processes";
+        }
+    }
+}
---
+++ src/AnyStatus.Plugins/Widgets/Metrics/Threads.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnyStatus.Plugins.Widgets.Metrics
+{
+    [DisplayColumn("Metrics")]
+    [DisplayName("Threads")]
+    [Description("The number of threads running on local computer.")]
+    public class Threads : PerformanceCounter
+    {
+        public Threads()
+        {
+            CategoryName = "System";
+            CounterName = "Threads";
+        }
+    }
+}
