f7e9775a902a2519f188fe4855a587d51e204d31
Toby Smith <tobysmith568@hotmail.co.uk>
2019-06-04 22:14:01 +0100

    Feature: RAM Usage #134

---
+++ src/AnyStatus.Plugins/Widgets/Metrics/RAM/Usage/RamUsage.cs
@@ -0,0 +1,21 @@
+﻿using AnyStatus.API;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnyStatus
+{
+    [DisplayColumn("Metrics")]
+    [DisplayName("RAM Usage")]
+    [Description("Shows the percentage of RAM usage for the local machine")]
+    public class RamUsage : Sparkline, ISchedulable
+    {
+        public RamUsage()
+        {
+            Symbol = "%";
+            Interval = 10;
+            Units = IntervalUnits.Seconds;
+            Name = "RAM Usage";
+            MaxValue = 100;
+        }
+    }
+}
