5d4838252c775707e6d79187190587de6b164f38
Toby Smith <tobysmith568@hotmail.co.uk>
2019-06-04 23:48:07 +0100

    Feature: Thread Count #136

---
+++ src/AnyStatus.Plugins/Widgets/Metrics/ThreadCount/ThreadCount.cs
@@ -0,0 +1,25 @@
+﻿using AnyStatus.API;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnyStatus
+{
+    [DisplayColumn("Metrics")]
+    [DisplayName("Thread Count")]
+    [Description("Shows the number of running CPU threads")]
+    public class ThreadCount : Sparkline, ISchedulable
+    {
+        public ThreadCount()
+        {
+            Symbol = "";
+            Interval = 10;
+            Units = IntervalUnits.Seconds;
+            Name = "Thread Count";
+        }
+
+        [Category("Thread Count")]
+        [DisplayName("Machine Name")]
+        [Description("Optional. Leave blank for local computer.")]
+        public string MachineName { get; set; }
+    }
+}
