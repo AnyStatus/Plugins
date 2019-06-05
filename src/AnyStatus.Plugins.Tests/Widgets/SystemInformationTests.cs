709832d7b51153c9576f5c8d2d496d55db008b07
Alon Amsalem <alon@anystat.us>
2019-06-05 22:19:40 +0300

    system information tests

---
+++ src/AnyStatus.Plugins.Tests/Widgets/SystemInformationTests.cs
@@ -0,0 +1,84 @@
+﻿using AnyStatus.API;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AnyStatus.Plugins.Tests.Widgets
+{
+    [TestClass]
+    public class SystemInformationTests
+    {
+        [TestMethod]
+        public async Task CpuUsageTest()
+        {
+            var widget = new CpuUsage();
+
+            var request = MetricQueryRequest.Create(widget);
+
+            var handler = new CpuUsageQuery();
+
+            await handler.Handle(request, CancellationToken.None).ConfigureAwait(false);
+
+            Assert.IsNotNull(widget.Value);
+        }
+
+        [TestMethod]
+        public async Task ProcessCpuUsageTest()
+        {
+            var widget = new ProcessCpuUsage()
+            {
+                ProcessName = "System",
+            };
+
+            var request = MetricQueryRequest.Create(widget);
+
+            var handler = new ProcessCpuUsageQuery();
+
+            await handler.Handle(request, CancellationToken.None).ConfigureAwait(false);
+
+            Assert.IsNotNull(widget.Value);
+        }
+
+        [TestMethod]
+        public async Task RamUsageTest()
+        {
+            var widget = new RamUsage();
+
+            var request = MetricQueryRequest.Create(widget);
+
+            var handler = new RamUsageQuery();
+
+            await handler.Handle(request, CancellationToken.None).ConfigureAwait(false);
+
+            Assert.IsTrue(widget.Value > 0);
+        }
+
+        [TestMethod]
+        public async Task ProcessCountTest()
+        {
+            var widget = new ProcessCount();
+
+            var request = MetricQueryRequest.Create(widget);
+
+            var handler = new ProcessCountQuery();
+
+            await handler.Handle(request, CancellationToken.None).ConfigureAwait(false);
+
+            Assert.IsTrue(widget.Value > 0);
+        }
+
+        [TestMethod]
+        public async Task ThreadCountTest()
+        {
+            var widget = new ThreadCount();
+
+            var request = MetricQueryRequest.Create(widget);
+
+            var handler = new ThreadCountQuery();
+
+            await handler.Handle(request, CancellationToken.None).ConfigureAwait(false);
+
+            Assert.IsTrue(widget.Value > 0);
+        }
+    }
+}
