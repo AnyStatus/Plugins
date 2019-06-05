284fdeb2c0c3eb5dcd6fdd51aee17fff85fb237c
AlonAm <alon.amsalem@gmail.com>
2019-06-05 11:54:19 +0300

    Update RamUsageQuery.cs

--- src/AnyStatus.Plugins/Widgets/Metrics/RAM/Usage/RamUsageQuery.cs
+++ src/AnyStatus.Plugins/Widgets/Metrics/RAM/Usage/RamUsageQuery.cs
@@ -8,13 +8,13 @@ namespace AnyStatus
     public class RamUsageQuery : IMetricQuery<RamUsage>
     {
         [DebuggerStepThrough]
-        public async Task Handle(MetricQueryRequest<RamUsage> request, CancellationToken cancellationToken)
+        public Task Handle(MetricQueryRequest<RamUsage> request, CancellationToken cancellationToken)
         {
-            await Task.Delay(1000).ConfigureAwait(false);
-
             request.DataContext.Value = (int)RamInformation.GetPercentageOfMemoryInUseMiB();
 
             request.DataContext.State = request.DataContext.Value == -1 ? State.Error : State.Ok;
+            
+            return Task.CompletedTask;
         }
     }
 }
