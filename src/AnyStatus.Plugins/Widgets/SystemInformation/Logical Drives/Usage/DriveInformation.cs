c14b6ddb701652847072d7b6d276e55ed4dfebdd
Toby Smith <tobysmith568@hotmail.co.uk>
2019-06-18 22:59:15 +0100

    Added tooltip to drive usage

--- src/AnyStatus.Plugins/Widgets/SystemInformation/Logical Drives/Usage/DriveInformation.cs
+++ src/AnyStatus.Plugins/Widgets/SystemInformation/Logical Drives/Usage/DriveInformation.cs
@@ -6,6 +6,41 @@ namespace AnyStatus
     public class DriveInformation
     {
         private const string Kernel32 = "kernel32.dll";
+        private const int GigabyteFactor = 1024 * 1024 * 1024;
+
+        public string Drive { get; private set; }
+        public ulong TotalNumberOfFreeBytes { get; private set; }
+        public ulong TotalNumberOfBytes { get; private set; }
+
+        public int TotalNumberOfGigabytes
+        {
+            get => (int)Math.Round((double)TotalNumberOfBytes / GigabyteFactor);
+        }
+
+        public int TotalNumberOfFreeGigabytes
+        {
+            get => (int)Math.Round((double)TotalNumberOfFreeBytes / GigabyteFactor);
+        }
+
+        public int TotalNumberOfUsedGigabytes
+        {
+            get => (int)Math.Round((double)(TotalNumberOfBytes - TotalNumberOfFreeBytes) / GigabyteFactor);
+        }
+
+        public int AvailablePercentage
+        {
+            get => (int)Math.Round((TotalNumberOfFreeBytes / (double)TotalNumberOfBytes) * 100);
+        }
+
+        public int UsedPercentage
+        {
+            get => (int)Math.Round(((TotalNumberOfBytes - TotalNumberOfFreeBytes) / (double)TotalNumberOfBytes) * 100);
+        }
+
+        private DriveInformation()
+        {
+
+        }
 
         [DllImport(Kernel32, SetLastError = true, CharSet = CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
@@ -14,32 +49,19 @@ namespace AnyStatus
                                                       [Out] out ulong totalNumberOfBytes,
                                                       [Out] out ulong totalNumberOfFreeBytes);
 
-        public static int GetDriveAvailablePercentage(string drive)
+        public static DriveInformation ReadDrive(string drive)
         {
-            ulong totalNumberOfFreeBytes;
-            ulong totalNumberOfBytes;
-
-            if (!GetDiskFreeSpaceEx(drive, out _, out totalNumberOfBytes, out totalNumberOfFreeBytes))
+            if (!GetDiskFreeSpaceEx(drive, out _, out ulong totalNumberOfBytes, out ulong totalNumberOfFreeBytes))
             {
-                return -1;
+                return null;
             }
 
-            return (int)Math.Round((totalNumberOfFreeBytes / (double)totalNumberOfBytes) * 100);
-        }
-
-        public static int GetDriveUsedPercentage(string drive)
-        {
-            ulong totalNumberOfFreeBytes;
-            ulong totalNumberOfBytes;
-
-            if (!GetDiskFreeSpaceEx(drive, out _, out totalNumberOfBytes, out totalNumberOfFreeBytes))
+            return new DriveInformation()
             {
-                return -1;
-            }
-
-            ulong totalNumberOfUsedBytes = totalNumberOfBytes - totalNumberOfFreeBytes;
-
-            return (int)Math.Round((totalNumberOfUsedBytes / (double)totalNumberOfBytes) * 100);
+                Drive = drive,
+                TotalNumberOfBytes = totalNumberOfBytes,
+                TotalNumberOfFreeBytes = totalNumberOfFreeBytes
+            };
         }
     }
 }
--- src/AnyStatus.Plugins/Widgets/SystemInformation/Logical Drives/Usage/LogicalDriveUsageQuery.cs
+++ src/AnyStatus.Plugins/Widgets/SystemInformation/Logical Drives/Usage/LogicalDriveUsageQuery.cs
@@ -8,24 +8,43 @@ namespace AnyStatus
 {
     public class LogicalDriveUsageQuery : IMetricQuery<LogicalDriveUsage>
     {
+        private static readonly string NL = Environment.NewLine;
+
         [DebuggerStepThrough]
         public async Task Handle(MetricQueryRequest<LogicalDriveUsage> request, CancellationToken cancellationToken)
         {
-            request.DataContext.Progress = GetDriveUsageAsync(request.DataContext.Drive, request.DataContext.PercentageType);
+            DriveInformation driveInformation = DriveInformation.ReadDrive(request.DataContext.Drive);
 
+            request.DataContext.Progress = GetDrivePercentage(driveInformation, request.DataContext.PercentageType);
+            request.DataContext.Message = GetDriveMessage(driveInformation, request.DataContext.PercentageType);
             request.DataContext.State = State.Ok;
 
             await Task.CompletedTask.ConfigureAwait(false);
         }
 
-        private int GetDriveUsageAsync(string drive, PercentageType percentageType)
+        private int GetDrivePercentage(DriveInformation driveInformation, PercentageType percentageType)
         {
             switch (percentageType)
             {
                 case PercentageType.PercentageUsed:
-                    return DriveInformation.GetDriveUsedPercentage(drive);
+                    return driveInformation.UsedPercentage;
                 case PercentageType.PercentageRemaining:
-                    return DriveInformation.GetDriveAvailablePercentage(drive);
+                    return driveInformation.AvailablePercentage;
+                default:
+                    throw new NotImplementedException($"Not implemented the percentage type of [{percentageType}]");
+            }
+        }
+
+        private string GetDriveMessage(DriveInformation driveInformation, PercentageType percentageType)
+        {
+            switch (percentageType)
+            {
+                case PercentageType.PercentageUsed:
+                    return $"{driveInformation.Drive} - {driveInformation.UsedPercentage}%{NL}" +
+                           $"{driveInformation.TotalNumberOfUsedGigabytes}GB used out of {driveInformation.TotalNumberOfGigabytes}GB";
+                case PercentageType.PercentageRemaining:
+                    return $"{driveInformation.Drive} - {driveInformation.AvailablePercentage}%{NL}" +
+                           $"{driveInformation.TotalNumberOfFreeGigabytes}GB available out of {driveInformation.TotalNumberOfGigabytes}GB";
                 default:
                     throw new NotImplementedException($"Not implemented the percentage type of [{percentageType}]");
             }
