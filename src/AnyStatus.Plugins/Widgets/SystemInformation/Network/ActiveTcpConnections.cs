ce8af6e28b8510722f100768c94869ba05cb62b1
Alon Amsalem <alon@anystat.us>
2019-06-21 00:01:37 +0300

    active tcp connections widget anystatus/support#148

---
+++ src/AnyStatus.Plugins/Widgets/SystemInformation/Network/ActiveTcpConnections.cs
@@ -0,0 +1,29 @@
+﻿using AnyStatus.API;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Net.NetworkInformation;
+
+namespace AnyStatus.Plugins.Widgets.SystemInformation.Network
+{
+    [DisplayColumn("System Information")]
+    [DisplayName("Active TCP Connections")]
+    [Description("View the number of active TCP connections on the local computer.")]
+    public class ActiveTcpConnections : Sparkline, ISchedulable
+    {
+        public ActiveTcpConnections()
+        {
+            Interval = 30;
+            Units = IntervalUnits.Seconds;
+        }
+    }
+
+    public class ActiveTcpConnectionsQuery : RequestHandler<MetricQueryRequest<ActiveTcpConnections>>
+    {
+        protected override void HandleCore(MetricQueryRequest<ActiveTcpConnections> request)
+        {
+            request.DataContext.Value = IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpConnections().Length;
+
+            request.DataContext.State = State.Ok;
+        }
+    }
+}
